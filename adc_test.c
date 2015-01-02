735e27fafcbddc0365aad2c1f02870eaf1cdaa7a
Evgenij <fluffy27@nxt.ru>
2015-01-02 22:01:20 +0200

    God, save Vadim

--- adc_test.c
+++ adc_test.c
@@ -1,7 +1,7 @@
 /*
  *	Testing ADC
  *
- * 	Measuring voltage on variable resistor and displaying 7-segments indicators
+ * 	Measuring voltage on variable resistor and displaying on 7-segments indicators
  * 	 
  *
  *	D12-D4 - Segments
@@ -17,28 +17,26 @@
 #define NUM_DIG 10
 
 // Interrupt when ADC complete conversion
-U16 adc_data = 0;
-U16 adc_tmp  = 0;
-
-U16 adc_res[4] = {0,0,0,0};
-unsigned char d = 0x01;
-
+float adc_data = 0;
+char adc_res[4] = {0,0,0,0};
+float adc_tmp = 0;
 ISR(ADC_vect)
 {
-	d = ADCL;
-	adc_res[3] = 4;
-//	PRR = 0x00;
+
+	adc_data = ADCL;
+	adc_data |= (ADCH<<8);
+
 }
 
 // 5ms interval
 
 char j=0;
-unsigned char k = 0;
+
 //unsigned char sec = 0;
 
 ISR(TIMER0_COMPA_vect)
 {
-	k++;
+	
 	PORTC = (1<<j);
 	j++;
 	if(j>3)
@@ -64,10 +62,8 @@ struct digit {
 	0x04, 0x00
 };
 
-//U16 adc_res[4] = {0,0,0,0};
-
 int main() {
-
+	
 	DDRC = 0x0F;
 	DDRB = 0x1F;
 	DDRD = 0xE0;
@@ -91,7 +87,7 @@ int main() {
 			REFS1:0=01 	-- 	Ref source connected to AVCC pin with 
 						 	capasitor in AREF pin
 			MUX3:0=0101 -- 	5 Channel in ADC
-			ADLAR=0		--	Left adjusted result in Data Registers (ADCH & ADCL)
+			ADLAR=1		--	Left adjusted result in Data Registers (ADCH & ADCL)
 
 		ADCSRA:
 			ADPS2:0=111	--	Source to ADC is CLK/128
@@ -101,60 +97,31 @@ int main() {
 			ADEN=1 		--	Enable ADC	
 			ADSC=1		--	Start conversion
 		PRR:
-			PRADC=0 	-- Power Reduction Off		
+			PRADC=0 	-- Power Reduciton Off		
 	*/
-	PRR    = 0<<PRADC;
-	ADMUX  = 5;
-	ADCSRA = 1 << ADEN;
-	ADCSRA = (1 << ADPS0) | (1<<ADPS1) | (1<<ADPS2);
-	ADMUX  = 1<<REFS0;
-	ADCSRA = 1<<ADIE;
-	ADCSRA = 1<<ADATE;
-	ADCSRA = 1<<ADSC;
+	
+	ADMUX   = (5<<MUX0);
+	ADMUX  |= (1<<REFS0);
+	ADMUX  |= (1<<ADLAR);
 
-
+	ADCSRA  = (7<<ADPS0);
+	ADCSRA |= (1<<ADIE);
+	ADCSRA |= (1<<ADATE);
+	ADCSRA |= (1<<ADEN);
+	ADCSRA |= (1<<ADSC);
 
 	// Allow global interrupt
 	sei();
 
-	//char t = 0;
+	U16 i = 0;
 	while(1)
 	{
-		//PRR=0<<PRADC;
 		PORTB = digits[adc_res[j]].PB;
-		PORTD = digits[adc_res[j]].PD;
-			
-/*		
-		if((adc_data = ((int)adc_data/(float)10000)) != 0) {
-			adc_res[0] = adc_data;
-
-			if((adc_data = ((int)adc_data/(float)1000)) != 0)
-			{
-				adc_res[1] = adc_data;
-
-				if((adc_data = ((int)adc_data/(float)100)) != 0)
-				{
-					adc_res[2] = adc_data;
-
-					if((adc_data = ((int)adc_data/(float)10)) != 0)
-					{
-						adc_res[3] = adc_data;
-					}
-				}
-			}
-		}
+		PORTD = digits[adc_res[j]].PD;	
 		
-*/
-/*		// Set ADIE & ADSC
-		if(ADCSRA & (1<<ADIF))
-			adc_res[2]++;
-		PRR = (0<<PRADC);
-		ADCSRA = (1<<ADIE);
-		ADCSRA = (1<<ADSC);		
-		if(ADCH > 0x00)
-			adc_res[0] = 8;
+		
 
-*/			
 
+				
 	}
 }
